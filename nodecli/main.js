4a5406a33109e56b12ee065e96eeb6fd45870ce8
ryotaro-kitamura <k.ryoutaro.g@gmail.com>
2020-05-09 12:53:10 +0900

    module の呼び出し

--- nodecli/main.js
+++ nodecli/main.js
@@ -1,1 +1,10 @@
-console.log(process.argv)
+// コマンドライン引数の取得
+console.log(process.argv);
+
+// myModule を呼び出す
+const myModule = require('./my-module');
+console.log(myModule.foo);
+
+// commander module を呼び出す
+const commander = require('commander');
+console.log(commander);
