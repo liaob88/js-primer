2b9b724e54bef6bf5f874d21ab7c3da2d782dc45
ryotaro-kitamura <k.ryoutaro.g@gmail.com>
2020-05-09 21:25:45 +0900

    Item を登録でいるところまで実装

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -1,11 +1,21 @@
-console.log('App.js: loaded');
+import { element } from './view/html-util.js';
+
 export class App {
   mount() {
     const formElement = document.querySelector('#js-form');
     const inputElement = document.querySelector('#js-form-input');
+    const containerElement = document.querySelector('#js-todo-list');
+    const todoItemCountElement = document.querySelector('#js-todo-count');
+
+    let todoItemCount = 0;
+
     formElement.addEventListener('submit', () => {
       event.preventDefault();
-      console.log(`入力欄の値:${inputElement.value}`);
+      const todoItemElement = element`<li>${inputElement.value}</li>`;
+      containerElement.appendChild(todoItemElement);
+      todoItemCount += 1;
+      todoItemCountElement.textContent = `Todoアイテム数: ${todoItemCount}`;
+      inputElement.value = '';
     });
   }
 }
