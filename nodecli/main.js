fead4a32d08f6decc75ee8d293d76d62d6e9a17d
ryotaro-kitamura <k.ryoutaro.g@gmail.com>
2020-05-09 13:26:02 +0900

    file 読み込みと標準出力

--- nodecli/main.js
+++ nodecli/main.js
@@ -1,18 +1,41 @@
 /**
- *  
- * 
+ *
+ *
+ * ファイル読み込み
+ *
+ *
+ */
+
+const program = require('commander');
+const fs = require('fs');
+
+program.parse(process.argv);
+const filePath = program.args[0];
+
+fs.readFile(filePath, { encoding: 'utf8' }, (err, file) => {
+  if (err) {
+    console.log(err.message);
+    process.exit(1);
+    return;
+  }
+  console.log(file);
+});
+
+/**
+ *
+ *
  * コマンドライン引数からファイルパスを取得する
- * 
- * 
-*/
+ *
+ *
+ */
 
-const commander = require('commander');
-commander.parse(process.argv);
+// const commander = require('commander');
+// commander.parse(process.argv);
 
-/** 
+/**
  * このプログラムを引数を渡した状態で実行すると、commander の中にある args[] に引数の文字列が配列で保存される。
  * 以下を試すとわかる。
-*/
+ */
 // console.log(commander);
 
 /**
@@ -20,6 +43,6 @@ commander.parse(process.argv);
  * commander を使用して取得すると、process.argv で取得した場合とは異なり、
  * node の実行プロセスパスと実行されたスクリプトファイルのパスは出力されない。
  * */
-const filePath = commander.args[0];
-console.log('commander あり', filePath);
-console.log('commander なし', process.argv);
+// const filePath = commander.args[0];
+// console.log('commander あり', filePath);
+// console.log('commander なし', process.argv);
